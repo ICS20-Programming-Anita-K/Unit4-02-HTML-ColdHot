be51b6d86bae4bee37ae481c497e1c94903103f1
Anita Kay <99274242+anitak456@users.noreply.github.com>
2022-04-30 14:24:57 +0000

    index.html

---
+++ js/script.js
@@ -0,0 +1,24 @@
+// Created by: Anita Kay
+// Created on: April 2022
+// This file contains the JS functions for index.html
+
+"use strict"
+
+/**
+ * This function displays a message when the user inputs the temperature
+ */
+function displayMessage () {
+	// get user input
+	let temp = parseInt(document.getElementById('temp').value)
+
+  	// if the temperature is below 15 degrees, display "It is cold outside!"
+	if (temp < 15) {
+		document.getElementById('message').innerHTML = "It is cold outside!"
+	} 
+	// otherwise, if the is greater than or equal to 15 degrees, display "It is warm outside!"	
+	else (temp >= 15) {
+		document.getElementById('message').innerHTML = "It is warm outside!"
+		}
+  
+  // display the message
+}
